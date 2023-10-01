0919f5cd3d131c7f84cd38d8974ba7cf5e15b72a
Marko Malenic <mmalenic1@gmail.com>
2023-10-01 11:08:53 +1100

    feat: gene combinations

--- dataset/load_protect.py
+++ dataset/load_protect.py
@@ -2,6 +2,7 @@ import os
 from typing import List
 
 import pandas as pd
+import itertools
 
 from classifer.util import find_file
 from dataset.mutation_landscape_cancer_type import MutationLandscapeCancerType
@@ -32,9 +33,7 @@ class LoadProtect:
         self._sample_dir = sample_dir
 
         self._df = None
-        self._stats_after_concat = None
-        self._stats_after_on_label_removed = None
-        self._stats_after_level_removed = None
+        self._stats = None
 
     def df(self) -> pd.DataFrame:
         """
@@ -46,25 +45,11 @@ class LoadProtect:
         return self._df
 
     @property
-    def stats_after_concat(self) -> pd.DataFrame:
+    def stats(self) -> pd.DataFrame:
         """
-        Get the stats after concat.
+        Get the stats.
         """
-        return self._stats_after_concat
-
-    @property
-    def stats_after_on_label_removed(self) -> pd.DataFrame:
-        """
-        Get the stats after onlabel removed.
-        """
-        return self._stats_after_on_label_removed
-
-    @property
-    def stats_after_level_removed(self) -> pd.DataFrame:
-        """
-        Get the stats after level removed.
-        """
-        return self._stats_after_level_removed
+        return self._stats
 
     def load(self) -> pd.DataFrame:
         """
@@ -78,6 +63,79 @@ class LoadProtect:
                 for url in urls
                 if "pubmed.ncbi.nlm.nih.gov" in url or "ncbi.nlm.nih.gov/pubmed" in url
             ]
+
+        def gene_combinations(x) -> pd.DataFrame:
+            x = x.astype("str")
+
+            x["temp"] = (
+                x["gene"]
+                + ";"
+                + x["transcript"]
+                + ";"
+                + x["isCanonical"]
+                + ";"
+                + x["event"]
+                + ";"
+                + x["eventIsHighDriver"]
+                + ";"
+                + x["germline"]
+                + ";"
+                + x["reported"]
+                + ";"
+                + x["treatment"]
+                + ";"
+                + x["onLabel"]
+                + ";"
+                + x["level"]
+                + ";"
+                + x["direction"]
+                + ";"
+                + x["sources"]
+                + ";"
+                + x["cancer_type"]
+                + ";"
+                + x["treatment_with_source"]
+            )
+
+            new = pd.DataFrame([y for y in itertools.combinations(x.temp, 2)])
+            if new.empty:
+                return pd.DataFrame()
+
+            new["temp"] = new[0] + ";" + new[1]
+
+            output = new["temp"].str.split(";", expand=True)
+            output.columns = [
+                "gene_x",
+                "transcript_x",
+                "isCanonical_x",
+                "event_x",
+                "eventIsHighDriver_x",
+                "germline_x",
+                "reported_x",
+                "treatment_x",
+                "onLabel_x",
+                "level_x",
+                "direction_x",
+                "sources_x",
+                "cancer_type_x",
+                "treatment_with_source_x",
+                "gene_y",
+                "transcript_y",
+                "isCanonical_y",
+                "event_y",
+                "eventIsHighDriver_y",
+                "germline_y",
+                "reported_y",
+                "treatment_y",
+                "onLabel_y",
+                "level_y",
+                "direction_y",
+                "sources_y",
+                "cancer_type_y",
+                "treatment_with_source_y",
+            ]
+
+            return output
 
         dfs = {}
         for sample in os.listdir(self._sample_dir):
@@ -102,6 +160,9 @@ class LoadProtect:
 
                 frame = pd.read_table(protect_file, sep="\t")
 
+                if frame.empty:
+                    continue
+
                 doid = protect_dir.split("_", 1)[1]
                 cancer_type = self._cancer_types.cancer_type(doid)
                 frame["cancer_type"] = cancer_type
@@ -110,6 +171,19 @@ class LoadProtect:
 
                 frame["treatment_with_source"] = list(
                     zip(frame["treatment"], frame["sources"])
+                )
+
+                frame = frame[frame["onLabel"]]
+                # frame = frame[(frame["level"] == "A") | (frame["level"] == "B")]
+
+                if frame.empty:
+                    continue
+
+                frame = frame.groupby(["cancer_type", "gene"]).agg(list).reset_index()
+                frame = (
+                    frame.groupby(["cancer_type"])
+                    .apply(gene_combinations)
+                    .reset_index()
                 )
 
                 df[protect_dir] = frame
@@ -123,13 +197,7 @@ class LoadProtect:
         print("number of samples:", len(dfs))
 
         output = pd.concat(dfs)
-        self._stats_after_concat = output.describe()
-
-        output = output[output["onLabel"]]
-        self._stats_after_on_label_removed = output.describe()
-
-        output = output[(output["level"] == "A") | (output["level"] == "B")]
-        self._stats_after_level_removed = output.describe()
+        self._stats = output.describe()
 
         self._df = output
 
