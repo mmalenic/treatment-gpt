21f83c99240de1267f20804960e3a9cccd869a83
Marko Malenic <mmalenic1@gmail.com>
2023-10-04 08:30:02 +1100

    feat: add treatment source dataset

--- dataset/gene_pair_dataset.py
+++ dataset/gene_pair_dataset.py
@@ -9,10 +9,11 @@ class GenePairDataset:
     Loads all protect data.
     """
 
-    protect_ending = ".protect.tsv"
     random_state = 42
 
-    def __init__(self, from_protect: LoadProtect, **kwargs) -> None:
+    def __init__(
+        self, from_protect: LoadProtect, n_treatment_shuffles: int = 3, **kwargs
+    ) -> None:
         """
         Initialize this class.
 
@@ -21,36 +22,47 @@ class GenePairDataset:
         self.__dict__.update(kwargs)
 
         self._dataset = []
-        self._additional_context = []
         self._from_protect = from_protect
+        self._n_treatment_shuffles = n_treatment_shuffles
 
         random.seed(self.random_state)
 
     def load(self):
-        """Load the dataset."""
-        for index, row in self._from_protect.df().iterrows():
-            self._additional_context.append(row.tolist())
+        """
+        Load the dataset.
+        """
 
+        for index, row in self._from_protect.df().iterrows():
             treatments = (
                 row["treatment_with_text_sources_x"]
                 + row["treatment_with_text_sources_y"]
             )
-            treatments = [(x[0], x[1]) for x in treatments]
-
-            all_treatments = self._from_protect.treatments_and_sources()
-            all_treatments = list([x for x in all_treatments if x[0] not in treatments])
-            random.shuffle(all_treatments)
-
-            treatments += all_treatments[: len(treatments)]
+            treatments = [
+                {"treatment": x[0], "source": x[1], "level": x[2]} for x in treatments
+            ]
 
             y_true = [x for x, _, _ in treatments]
 
-            self.dataset.append(
-                {
-                    "cancer_type": row["cancer_type"],
-                    "gene_x": row["gene_x"],
-                    "gene_y": row["gene_y"],
-                    "treatments": treatments,
-                    "y_true": y_true,
-                }
-            )
+            all_treatments = self._from_protect.treatments_and_sources()
+            all_treatments = list([x for x in all_treatments if x[0] not in treatments])
+
+            for _ in range(self._n_treatment_shuffles):
+                random.shuffle(all_treatments)
+                treatments += all_treatments[: len(treatments)]
+
+                self._dataset.append(
+                    {
+                        "index": index,
+                        "cancer_type": row["cancer_type"],
+                        "gene_x": row["gene_x"],
+                        "gene_y": row["gene_y"],
+                        "treatments": treatments,
+                        "y_true": y_true,
+                    }
+                )
+
+    def dataset(self):
+        """
+        Return the dataset.
+        """
+        return self._dataset
---
+++ dataset/treatment_source_dataset.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from dataset.load_protect import LoadProtect
+import random
+
+
+class TreatmentSourceDataset:
+    """
+    Treatment source dataset
+    """
+
+    random_state = 42
+
+    def __init__(self, from_protect: LoadProtect, **kwargs) -> None:
+        """
+        Initialize this class.
+
+        :param from_protect: load protect data from this class.
+        """
+        self.__dict__.update(kwargs)
+
+        self._dataset = []
+        self._from_protect = from_protect
+
+        random.seed(self.random_state)
+
+    def load(self):
+        """
+        Load the dataset.
+        """
+
+        all_treatments = set()
+        for index, row in self._from_protect.df().iterrows():
+            treatments = (
+                row["treatment_with_text_sources_x"]
+                + row["treatment_with_text_sources_y"]
+            )
+            for treatment in treatments:
+                all_treatments.add((treatment[0], treatment[1]))
+
+        for treatment, source in all_treatments:
+            self._dataset.append(
+                {
+                    "source": source,
+                    "treatment": treatment,
+                }
+            )
+
+    def dataset(self):
+        """
+        Return the dataset.
+        """
+        return self._dataset
